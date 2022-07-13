03bf3c85824490b9da355cc285ae44dc98bc6926
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2022-07-13 09:48:04 +0300

    feat: create an example of AMQP producer app
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

---
+++ src/test/java/org/ujar/basics/amqp/producing/hello/producer/GreetingMessageProducerTest.java
@@ -0,0 +1,37 @@
+package org.ujar.basics.amqp.producing.hello.producer;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+
+import java.util.concurrent.TimeUnit;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.ujar.basics.amqp.producing.hello.consumer.GreetingMessageConsumer;
+import org.ujar.basics.amqp.producing.hello.model.Greeting;
+
+@SpringBootTest
+class GreetingMessageProducerTest {
+
+  private final GreetingMessageProducer producer;
+  private final GreetingMessageConsumer consumer;
+
+  @Autowired
+  public GreetingMessageProducerTest(GreetingMessageProducer producer, GreetingMessageConsumer consumer) {
+    this.producer = producer;
+    this.consumer = consumer;
+  }
+
+  @Test
+  void shouldConsumeMessage() throws InterruptedException {
+    var greeting = new Greeting("Hello, World!");
+    producer.send(greeting);
+
+    boolean messageConsumed = consumer.getLatch().await(10, TimeUnit.SECONDS);
+    assertThat(messageConsumed).isTrue();
+    MatcherAssert.assertThat(consumer.getPayload(), containsString(greeting.toString()));
+    consumer.resetLatch();
+  }
+}
