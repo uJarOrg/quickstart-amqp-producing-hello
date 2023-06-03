bdb062a9462a6003bd44bec3db9d038876696784
Dima Denysenko <coding@dimdnk.com>
2023-06-03 18:45:44 +0300

    chore: enable reasonable defaults for observability components

--- src/main/java/org/ujar/amqpproducinghello/AmqpProducingHelloApplication.java
+++ src/main/java/org/ujar/amqpproducinghello/AmqpProducingHelloApplication.java
@@ -2,10 +2,13 @@ package org.ujar.amqpproducinghello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class AmqpProducingHelloApplication {
   public static void main(String[] args) {
-    SpringApplication.run(AmqpProducingHelloApplication.class, args);
+    SpringApplication springApplication = new SpringApplication(AmqpProducingHelloApplication.class);
+    springApplication.setApplicationStartup(new BufferingApplicationStartup(2048));
+    springApplication.run(args);
   }
 }
